c0437e288fa5cd998fd15ceebf798549f86819b0
sajaa45 <moussa.saja@gmail.com>
2024-12-15 15:54:59 +0100

    final version

---
+++ jj.js
@@ -0,0 +1,225 @@
+
+function ch(){
+    const p= document.getElementById('searchB');
+    p.classList.add('ch');
+}
+let arr=[$('#food'),$('#chri'),$('#sold')];
+function pre(){
+    var but=document.getElementById('food');
+    i=arr.indexOf($('#food')[0]);
+    if (i > 0) {
+        but.src = arr[i - 1].attr('src'); // Use attr('src') to get the src attribute
+    } else {
+        but.src = arr[2].attr('src'); // Fix the typo, change 'scr' to 'src'
+    }
+}
+function next(){ 
+    let s=document.getElementsByClassName("n");
+    let i=s.item;
+    document.getElementById('food').src='chri.jpg';
+
+    
+}
+function cha(){
+    document.getElementById('cat').src='cato.jpg';
+    const j=document.getElementsByTagName("h2")[0];
+    const p=document.getElementsByTagName("p")[0];
+    const l=document.getElementById("pps");
+    j.classList.add('h');
+    p.classList.add('h');
+    l.classList.add('pp');
+}
+function prev(){
+    document.getElementById('food').src='sold.jpg';
+}
+function chi(){
+        document.getElementById('cat').src='cat.jpg';
+        var element = document.getElementById("pps");
+        element.classList.remove("pp");
+        const j=document.getElementsByTagName("h2")[0];
+        const p=document.getElementsByTagName("p")[0];
+        j.classList.remove('h');
+        p.classList.remove('h');
+}
+function cham(){
+    document.getElementById('mat').src='rabito.jpg';
+    const j=document.getElementsByTagName("h2")[1];
+    const p=document.getElementsByTagName("p")[1];
+    const l=document.getElementById("ppps");
+    j.classList.add('hh');
+    p.classList.add('hh');
+    l.classList.add('ppp');}
+function chim(){
+    {
+        document.getElementById('mat').src='rabit.jpg';
+        var element = document.getElementById("ppps");
+        element.classList.remove("ppp");
+        const j=document.getElementsByTagName("h2")[1];
+        const p=document.getElementsByTagName("p")[1];
+        j.classList.remove('hh');
+        p.classList.remove('hh');}
+}
+function chag(){
+    document.getElementById('dog').src='dog.jpg';
+    const j=document.getElementsByTagName("h2")[2];
+    const p=document.getElementsByTagName("p")[2];
+    const l=document.getElementById("pips");
+    j.classList.add('hhh');
+    p.classList.add('hhh');
+    l.classList.add('pppp');}
+function chig(){
+    {
+        document.getElementById('dog').src='doggo.jpg';
+        var element = document.getElementById("pips");
+        element.classList.remove("pppp");
+        const j=document.getElementsByTagName("h2")[2];
+        const p=document.getElementsByTagName("p")[2];
+        j.classList.remove('hhh');
+        p.classList.remove('hhh');}
+}
+function handleClic() {
+    alert('unvalid research,Try again');
+  }
+function handleClick() {
+    alert('Thank you for telling us your problem.We will contact you soon!');
+  }
+  function chag(){
+    document.getElementById('dog').src='dog.jpg';
+    const j=document.getElementsByTagName("h2")[2];
+    const p=document.getElementsByTagName("p")[2];
+    const l=document.getElementById("pips");
+    j.classList.add('hhh');
+    p.classList.add('hhh');
+    l.classList.add('pppp');}
+function chax(){
+    {
+        var element = document.getElementById("1");
+        element.classList.add("ai");
+        const j=document.getElementsByTagName("h6")[0];
+        const p=document.getElementsByTagName("h4")[0];
+        j.classList.add('h6');
+        p.classList.add('h6');}
+}
+function chix()
+    {
+        var element = document.getElementById("1");
+        element.classList.remove("ai");
+        const j=document.getElementsByTagName("h6")[0];
+        const p=document.getElementsByTagName("h4")[0];
+        j.classList.remove('h6');
+        p.classList.remove('h6');}
+function removeChix() {
+      // Remove the chix function from the onmouseout event
+      document.getElementById('1').removeAttribute('onmouseout');
+    }
+function chax1(){
+    {
+        var element = document.getElementById("2");
+        element.classList.add("ai");
+        const j=document.getElementsByTagName("h6")[1];
+        const p=document.getElementsByTagName("h4")[1];
+        j.classList.add('h6');
+        p.classList.add('h6');}
+}
+function chix1()
+    {
+        var element = document.getElementById("2");
+        element.classList.remove("ai");
+        const j=document.getElementsByTagName("h6")[1];
+        const p=document.getElementsByTagName("h4")[1];
+        j.classList.remove('h6');
+        p.classList.remove('h6');}
+function removeChix1() {
+      // Remove the chix function from the onmouseout event
+      document.getElementById('2').removeAttribute('onmouseout');
+    }
+
+function chax2(){
+    {
+        var element = document.getElementById("3");
+        element.classList.add("ai");
+        const j=document.getElementsByTagName("h6")[2];
+        const p=document.getElementsByTagName("h4")[2];
+        j.classList.add('h6');
+        p.classList.add('h6');}
+}
+function chix2()
+    {
+        var element = document.getElementById("3");
+        element.classList.remove("ai");
+        const j=document.getElementsByTagName("h6")[2];
+        const p=document.getElementsByTagName("h4")[2];
+        j.classList.remove('h6');
+        p.classList.remove('h6');}
+function removeChix2() {
+      // Remove the chix function from the onmouseout event
+      document.getElementById('3').removeAttribute('onmouseout');
+    }
+
+function chax3(){
+    {
+        var element = document.getElementById("4");
+        element.classList.add("ai");
+        const j=document.getElementsByTagName("h6")[3];
+        const p=document.getElementsByTagName("h4")[3];
+        j.classList.add('h6');
+        p.classList.add('h6');}
+}
+function chix3()
+    {
+        var element = document.getElementById("4");
+        element.classList.remove("ai");
+        const j=document.getElementsByTagName("h6")[3];
+        const p=document.getElementsByTagName("h4")[3];
+        j.classList.remove('h6');
+        p.classList.remove('h6');}
+function removeChix3() {
+      // Remove the chix function from the onmouseout event
+      document.getElementById('4').removeAttribute('onmouseout');
+    }
+
+function chax4(){
+    {
+        var element = document.getElementById("5");
+        element.classList.add("ai");
+        const j=document.getElementsByTagName("h6")[4];
+        const p=document.getElementsByTagName("h4")[4];
+        j.classList.add('h6');
+        p.classList.add('h6');}
+}
+function chix4()
+    {
+        var element = document.getElementById("5");
+        element.classList.remove("ai");
+        const j=document.getElementsByTagName("h6")[4];
+        const p=document.getElementsByTagName("h4")[4];
+        j.classList.remove('h6');
+        p.classList.remove('h6');}
+function removeChix4() {
+      // Remove the chix function from the onmouseout event
+      document.getElementById('5').removeAttribute('onmouseout');
+    }
+function chax5(){
+    {
+        var element = document.getElementById("6");
+        element.classList.add("ai");
+        const j=document.getElementsByTagName("h6")[5];
+        const p=document.getElementsByTagName("h4")[5];
+        j.classList.add('h6');
+        p.classList.add('h6');}
+}
+function chix5()
+    {
+        var element = document.getElementById("6");
+        element.classList.remove("ai");
+        const j=document.getElementsByTagName("h6")[5];
+        const p=document.getElementsByTagName("h4")[5];
+        j.classList.remove('h6');
+        p.classList.remove('h6');}
+function removeChix5() {
+      // Remove the chix function from the onmouseout event
+      document.getElementById('6').removeAttribute('onmouseout');
+    }
+function buy(){
+    alert("Thank You For Your Order. We Will Contact You Soon!")
+}
